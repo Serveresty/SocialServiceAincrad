7c31349027e147df086433ebcc4ecba87ce77e7b
Serveresty <cs.go.12228@gmail.com>
2024-01-12 09:48:22 +0300

    reworked main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,30 +9,56 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func run() error {
+	dbConnection, err := connectToDatabase()
+	if err != nil {
+		return err
+	}
+
+	router := startRouter()
+
+	handlers.AllRequests(router, dbConnection)
+
+	router.Run(":8080")
+	return nil
+}
+
+func connectToDatabase() (*pgx.Conn, error) {
 	dbUrl, err := getfromjson.GetDatabaseConData()
 	if err != nil {
 		log.Fatalf("Error while getting database url: %v", err)
+		return nil, err
 	}
 
 	conn, err := database.DB_Init(dbUrl)
 	if err != nil {
 		log.Fatalf("Error while connecting to database: %v", err)
+		return nil, err
 	}
-
-	gin.SetMode(gin.ReleaseMode)
-
-	router := gin.New()
 
 	err = conn.Ping(context.Background())
 	if err != nil {
 		log.Fatal("db err")
+		return nil, err
 	}
 	fmt.Println("db works")
+	return conn, nil
+}
 
-	handlers.AllRequests(router)
+func startRouter() *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.New()
 
-	router.Run(":8080")
+	return router
 }
--- service/handlers/handlers.go
+++ service/handlers/handlers.go
@@ -5,9 +5,10 @@ import (
 	signup "SocialServiceAincrad/internal/sign_up"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
-func AllRequests(router *gin.Engine) {
+func AllRequests(router *gin.Engine, dbConn *pgx.Conn) {
 	router.GET("/sign-in", signin.SignIn)
 	router.GET("/sign-up", signup.SignUp)
 }
