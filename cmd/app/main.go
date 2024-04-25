e8c9022805823604bc73d3653ac33758d9d4ce3d
Serveresty <cs.go.12228@gmail.com>
2024-04-25 10:00:28 +0300

    allow origin

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -72,7 +72,7 @@ func startRouter() *gin.Engine {
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Range, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
