d91f21a42147587e04cbc97505927f8ae0edf27d
Serveresty <cs.go.12228@gmail.com>
2024-05-15 05:04:55 +0300

    added middleware

--- internal/requests/logout/logout.go
+++ internal/requests/logout/logout.go
@@ -1,12 +1,20 @@
 package logout
 
 import (
+	cerr "SocialServiceAincrad/custom_errors"
+	"SocialServiceAincrad/utils"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Logout(c *gin.Context) {
+	err := utils.CheckAlreadyToken(c)
+	if err == nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrUnauthorized.Error()})
+		return
+	}
+
 	c.Request.Header.Del("Authorization")
 	c.JSON(http.StatusOK, gin.H{"message": "Logout success"})
 }
