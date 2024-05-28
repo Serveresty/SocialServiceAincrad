19a1525ab4e747dd01275f2ef0388ca6a180bdbc
Serveresty <cs.go.12228@gmail.com>
2024-05-28 23:32:53 +0300

    get video list by user id handler

--- internal/requests/profile/video/video.go
+++ internal/requests/profile/video/video.go
@@ -1,9 +1,89 @@
 package video
 
-import "github.com/gin-gonic/gin"
+import (
+	cerr "SocialServiceAincrad/custom_errors"
+	profiledb "SocialServiceAincrad/internal/database/profile_db"
+	jwtservice "SocialServiceAincrad/internal/jwt-service"
+	"SocialServiceAincrad/utils"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func VideoGET(c *gin.Context) {
 }
 
 func VideoCurrentUserGET(c *gin.Context) {
+	err := utils.CheckAlreadyToken(c)
+	if err == nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrUnauthorized.Error()})
+		return
+	}
+
+	claims, err := jwtservice.ParseToken(c)
+	if err != nil {
+		c.JSON(http.StatusForbidden, gin.H{"error": err.Error()})
+		return
+	}
+
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if claims.Subject == id {
+		videos, err := profiledb.GetVideosListById(idInt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": videos})
+		return
+	} else {
+		privacy, err := profiledb.GetPrivacySettings(idInt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		if privacy.Video == "all" {
+			videos, err := profiledb.GetVideosListById(idInt)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"data": videos})
+			return
+		}
+
+		if privacy.Video == "friends" {
+			currentUserId, err := strconv.Atoi(claims.Subject)
+			if err != nil {
+				c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrorClaims.Error()})
+				return
+			}
+			ok, err := profiledb.IsFriendOneByOne(idInt, currentUserId)
+			if err != nil {
+				c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrNoAccessByPrivacy.Error()})
+				return
+			}
+			if !ok {
+				c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrNoAccessByPrivacy.Error()})
+				return
+			}
+			videos, err := profiledb.GetVideosListById(idInt)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+			c.JSON(http.StatusOK, gin.H{"data": videos})
+			return
+		} else {
+			c.JSON(http.StatusForbidden, gin.H{"error": cerr.ErrNoAccessByPrivacy.Error()})
+			return
+		}
+	}
 }
