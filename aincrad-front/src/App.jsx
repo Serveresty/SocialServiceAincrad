ef65be1e09f5a3b0d5dcb5a94dc81e6efb377a5b
Serveresty <cs.go.12228@gmail.com>
2024-06-07 18:39:43 +0300

    reworked routes

--- aincrad-front/src/App.jsx
+++ aincrad-front/src/App.jsx
@@ -4,36 +4,18 @@ import { Filecontext } from './contexts/Filecontext';
 import './App.css'
 import AuthComponent from './pages/Auth';
 import RegistrationComponent from './pages/Registration';
-import EmptyComponent from './pages/Main';
-import ProfileComponent from './pages/Profile';
-import FriendsComponent from './pages/Friends';
-import AudioGETComponent from './pages/Audio';
-import Header from './components/Header';
-import LeftBar from './components/LeftBar';
-import Chat from './pages/Chat';
-import LogoutComponent from './pages/Logout';
-import VideoGrid from './pages/Video';
+import MainLayout from './MainApp';
 
 function App() {
   const [logID, setLogID] = useState("")
   return (
     <div>
       <Filecontext.Provider value={{ logID, setLogID }}>
-        <Header />
-        <div className="main-container">
-        <LeftBar />
         <Routes>
-          <Route path="/" element={<EmptyComponent />}/>
+          <Route path="*" element={<MainLayout />}/>
           <Route path="/sign-up" element={<RegistrationComponent />} />
           <Route path="/sign-in" element={<AuthComponent />} />
-          <Route path="/logout" element={<LogoutComponent />} />
-          <Route path="/:id" element={<ProfileComponent />} />
-          <Route path="/friends" element={<FriendsComponent />} />
-          <Route path="/audio" element={<AudioGETComponent />} />
-          <Route path="/messages" element={<Chat />} />
-          <Route path="/video/:id" element={<VideoGrid />} />
         </Routes>
-        </div>
       </Filecontext.Provider>
     </div>
   );
